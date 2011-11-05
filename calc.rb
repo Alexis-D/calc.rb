1dd11fb6b7dc3a7530a38bd1728ea20cb3415d0b
Alexis Daboville <alexis.daboville@gmail.com>
2011-11-05 16:27:58 +0000

    Use ARGF, plus refactor code.

--- calc.rb
+++ calc.rb
@@ -2,27 +2,38 @@
 
 load "parser.rb"
 
+# Just compute an expression per line
+# may be called like this:
+# $ echo "3 + 2" | ./calc.rb
+# $ 5
+# or like this (one expression per line
+# in each file):
+# $ ./calc.rb file{1..3}
+# $ # print results
+#
+
+# Evaluate each line of the file
+# Params:
+# +f+:: the file to evaluate
+def eval_file(f)
+  parser = Parser.new
+  f.each do |line|
+    begin
+      p Parser.new.eval line
+    rescue
+      $stderr.puts $!
+    end
+  end
+end
+
 if ARGV.size > 0
   ARGV.each do |filename|
-    parser = Parser.new
     File.open(filename) do |f|
-      while src = f.gets
-        begin
-          p parser.eval src
-        rescue
-          p $!
-        end
-      end
+      eval_file f
     end
   end
 
 else
-  while src = STDIN.read
-    begin
-      p Parser.new.eval src
-    rescue
-      p $!
-    end
-  end
+  eval_file(ARGF)
 end
 
